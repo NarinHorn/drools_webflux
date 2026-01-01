1c9c604b6b93c1dbe622837307f6bbee91aa944f
Horn Narin <135811993+NarinHorn@users.noreply.github.com>
2026-01-01 15:37:37 +0700

    prepare project structure

---
+++ src/main/java/com/hunesion/drools_v4_webflux/DroolsV4WebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.hunesion.drools_v4_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsV4WebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsV4WebfluxApplication.class, args);
+    }
+
+}
